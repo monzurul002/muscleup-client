7dc41e721aee21cc5fe8352e5b8917640a181564
monzurul002 <monzurul002@gmail.com>
2023-12-13 16:32:04 +0600

    instructor page

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -1,16 +1,64 @@
 
 const Instructor = () => {
     return (
-        <div style={{ backgroundColor: "#060e0e" }} className=" mx-auto pt-28 	 grid grid-cols-1 md:grid-cols-2">
-            <div className="card card-side bg-gradient-to-r from-cyan-200 from-10% via-cyan-300 via-30% to-rose-200	 to-90% shadow-xl rounded-lg">
-                <div className="w-50">
-                    <figure><img className="w-50 " src="" alt="Movie" /></figure>
+        <div className="grid grid-cols-1 pt-24 md:grid-cols-3 gap-4 w-11/12 mx-auto">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://rainbowit.net/html/histudy/assets/images/course/course-online-01.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Danel Handy</h2>
+                    <p>danelHandy@gmail.com</p>
+                    {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div> */}
                 </div>
-                <div className="card-body w-50">
-                    <h2 className="card-title">Danel Hand</h2>
-                    <p>danerl@yahoo.com</p>
-                    {/* <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://rainbowit.net/html/histudy/assets/images/course/course-online-05.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Danel Handy</h2>
+                    <p>danelHandy@gmail.com</p>
+                    {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div> */}
+                </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://rainbowit.net/html/histudy/assets/images/course/course-online-02.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Danel Handy</h2>
+                    <p>danelHandy@gmail.com</p>
+                    {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div> */}
+                </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://rainbowit.net/html/histudy/assets/images/course/course-online-01.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Danel Handy</h2>
+                    <p>danelHandy@gmail.com</p>
+                    {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div> */}
+                </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://rainbowit.net/html/histudy/assets/images/course/course-online-01.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Danel Handy</h2>
+                    <p>danelHandy@gmail.com</p>
+                    {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
                     </div> */}
                 </div>
             </div>
